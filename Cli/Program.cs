a5030d67c87e7498f58ef88d7f373197f30c23c0
Odepax <odepaxforuma@gmail.com>
2019-09-03 11:19:37 +0200

    Added more prototyping

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using Microsoft.CSharp;
 
 namespace Cli
 {
 	public static class Program
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
-			Console.WriteLine("Hello Nap!");
+			CodeCompileUnit compileUnit = new CodeCompileUnit
+			{
+				Namespaces =
+				{
+					new CodeNamespace("Hellow")
+					{
+						Types =
+						{
+							new CodeTypeDeclaration("Program")
+							{
+								Members =
+								{
+									new CodeEntryPointMethod
+									{
+										Statements =
+										{
+											new CodeMethodInvokeExpression(
+												new CodeTypeReferenceExpression("System.Console"),
+												"WriteLine",
+												new CodePrimitiveExpression("Hello World!")
+											)
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			};
+
+			using TextWriter file = new StreamWriter("Hellow.cs", false);
+			using CodeDomProvider code = new CSharpCodeProvider();
+
+			code.GenerateCodeFromCompileUnit(compileUnit, file, new CodeGeneratorOptions
+			{
+				IndentString = "\t",
+				BlankLinesBetweenMembers = true,
+				BracingStyle = "C",
+				ElseOnClosing = false,
+				VerbatimOrder = true
+			});
 		}
 	}
 }
