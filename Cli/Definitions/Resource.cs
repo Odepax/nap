6c4f99e2d7cb2431ec119f580ab1bbb393670f54
Odepax <odepaxforuma@gmail.com>
2019-09-03 12:14:58 +0200

    Added JSON input reader

---
+++ Cli/Definitions/Resource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Nap.Cli.Definitions
+{
+	public sealed class Resource
+	{
+		public string Name { get; }
+		public IReadOnlyDictionary<string, string> Fields { get; }
+
+		public Resource(string name, IReadOnlyDictionary<string, string> fields)
+		{
+			Name = name;
+			Fields = fields;
+		}
+	}
+}
---
+++ Cli/Json/InputReader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Nap.Cli.Definitions;
+
+namespace Nap.Cli.Json
+{
+	public static class InputReader
+	{
+		public static IReadOnlyList<Resource> ReadFromJson(Stream stream)
+		{
+			using JsonDocument json = JsonDocument.Parse(stream);
+
+			List<Resource> resources = new List<Resource>();
+
+			foreach (JsonProperty resourceProperty in json.RootElement.EnumerateObject())
+			{
+				string resourceName = resourceProperty.Name;
+				Dictionary<string, string> resourceFields = new Dictionary<string, string>();
+
+				foreach (JsonProperty fieldProperty in resourceProperty.Value.EnumerateObject())
+				{
+					string fieldName = fieldProperty.Name;
+					string fieldType = fieldProperty.Value.GetString();
+
+					resourceFields.Add(fieldName, fieldType);
+				}
+
+				resources.Add(new Resource(resourceName, resourceFields));
+			}
+
+			return resources;
+		}
+	}
+}
